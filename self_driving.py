1221f8954ef07ba45092ff4ada3ee58dd67fdde3
edwar203 <127564906+edwar203@users.noreply.github.com>
2023-05-12 05:38:13 -0600

    added more methods and flushed out console display

--- self_driving.py
+++ self_driving.py
@@ -9,26 +9,73 @@ import random
 from rich.console import Console
 from rich.panel import Panel
 
-console = Console
+console = Console()
 
 #Create Class
 class StantonAI():
 
 #Create Methods
     def __init__(self):
-        pass
+        self.speed = 0
+        self.running = 0
 
     def start(self):
-        pass
+        self.running = 1
+        self.speed = 5
+        console.print(
+                Panel.fit(f"Starting Moving Speed Increased to {self.speed} MPH"
+                          , style = "italic dark_red")
+            )
+        
 
     def stop(self):
-        pass
+        self.speed = 0
+        console.print(
+                Panel.fit("Now Stopping And Parking Vehicle"
+                          , style = "italic dark_red")
+            )
 
     def speedup(self):
-        pass
+        addspeed = console.input(
+                Panel.fit(f"You are currently going {self.speed}MPH \nHow Many MPH would yo like to add to your speed?"
+                          , style = "italic dark_red")
+            )
+        self.incspeed = self.speed + addspeed
 
     def slowdown(self):
+        print("works")
+
+    def decide(self):
+        if self.speed < 141:
+            self.display_console()
+        else:
+            self.random_event()
+
+    def random_event(self):
         pass
+
+
+    def display_console(self):
+        console.print(
+                Panel.fit("To Start Moving Please Input 1 \nTo Speed Up Please Input 2 \nTo Slow Down Please Input 3 \nTo Stop And Park Please Input 4 \n To Quit Please Input 5"
+                          , style = "italic dark_red")
+            )
+        choice = console.input(Panel.fit("What would you like to do? (1, 2, 3, 4, or 5): ", style = "italic dark_red"))
+
+        if choice == "1":
+            self.start()
+            self.decide()
+        elif choice == "2":
+            self.speedup()
+            self.decide()
+        elif choice == "3":
+            self.slowdown()
+            self.decide()
+        elif choice == "4":
+            self.stop()
+            self.decide()
+        elif choice == "5":
+            quit
 
 #Create Main
 def main():
@@ -39,7 +86,7 @@ def main():
     #run anything not in class
 
     #run class
-    self_drive = StantonAI
+    self_drive = StantonAI()
 
     self_drive.display_console()
 
